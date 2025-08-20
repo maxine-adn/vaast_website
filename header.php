8cbea0e1d11a51c23c923a50f2f0b417c334f09a
Maxine <maxine.audin@gmail.com>
2025-08-20 14:46:14 +0200

    Fixed header title moving

--- header.php
+++ header.php
@@ -1,13 +1,11 @@
 <div class="container-sm">
   <header class="border-bottom lh-1 py-3">
-    <div class="row flex-nowrap justify-content-between align-items-center">
-      <div class="col-4 pt-1"></div>
-      <div class="col-4 text-center">
+    <div class="row flex-nowrap align-items-center">
+      <div class="col-4 text-center mx-auto" style="width:auto">
         <a
           class="blog-header-logo text-body-emphasis text-decoration-none"
           href="index.php">The VAAST website</a>
       </div>
-      <div class="col-4 d-flex justify-content-end align-items-center"></div>
     </div>
   </header>
   <div class="nav-scroller py-1 mb-3 border-bottom">

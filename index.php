7786c8d157f8aee47f57388db8cbf41b379c5736
Maxine <maxine.audin@gmail.com>
2025-08-29 11:32:19 +0200

    different versions of the hero

--- index.php
+++ index.php
@@ -16,36 +16,223 @@
     rel="stylesheet" />
 </head>
 
-<body class="d-flex h-100 text-center">
+<body class="d-flex h-100"> <!-- .text-center -->
   <?php include 'color-modes.php'; ?>
-  <?php include 'header.php'; ?>
-  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-    <?php include 'header.php'; ?> <!-- c'est moooooche -->
-    <main class="px-3">
-      <h1>Cover your page.</h1>
+  <div class="cover-container d-flex w-100 h-100 mx-auto flex-column">
+    <?php include 'header.php'; ?>
+    <main class="container-sm px-3">
+
+      <div class="px-0 pt-4 my-5 text-center border-bottom">
+        <h2 class="mb-3">A highly replicable implicit measure of attitudes:</h2>
+        <h1 class="display-5 fw-bold text-body-emphasis mb-4" id="title">
+          &#10024; The Visual Approach and Avoidance by the Self Task &#10024;
+        </h1>
+        <br>
+        <div class="col-lg-12 mx-auto">
+          <p class="lead mb-2 mt-2">
+            Find out about the experiment, try it out and learn how to set it up for yourself! <br>
+            You can even choose the background, the stimuli and different features to make the experiment correspond to your needs. <br>
+            Expend the research field with a quality experiment right now!!
+          </p>
+        </div>
+        <div class="overflow-hidden pt-4" style="max-height: 50vh">
+          <div class="container px-5">
+            <img
+              src="remove-later/bootstrap-themes.png"
+              class="img-fluid border rounded-3 shadow-lg mb-4"
+              alt="Example image"
+              width="875"
+              height="625"
+              loading="lazy" />
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <br>
+
+      <div class="container-sm my-5">
+        <div
+          class="row p-4  pe-lg-0 pt-lg-5 pb-lg-5 align-items-center rounded-3 border shadow-lg">
+          <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+            <h1 class="display-4 fw-bold lh-1 text-body-emphasis">
+              A highly replicable implicit measure of attitudes
+
+            </h1>
+            <p class="lead">
+              Find out about the experiment, try it out and learn how to set it up for yourself! <br>
+              You can even choose the background, the stimuli and different features to make the experiment worrespond to your needs. <br>
+              Expend the research with a quality experiment right now!!
+            </p>
+          </div>
+          <div class="col-lg-5 offset-lg-0 p-0 overflow-hidden shadow-lg">
+            <img
+              class="rounded-lg-3"
+              src="remove-later/bootstrap-themes.png"
+              alt=""
+              width="720" />
+          </div>
+        </div>
+      </div>
+
+      <br><br>
+      <hr />
+
+      <div class="container-m col-xxl-12 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div class="col-10 col-sm-8 col-lg-6">
+            <img
+              src="remove-later/bootstrap-themes.png"
+              class="d-block mx-lg-auto img-fluid"
+              alt="Bootstrap Themes"
+              width="1400"
+              height="500"
+              loading="lazy" />
+          </div>
+          <div class="col-lg-6">
+            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">
+              A highly replicable implicit measure of attitudes via approach and avoidance movements:
+              <br>
+              &#10024;The Visual Approach and Avoidance by the Self Task&#10024;
+            </h1>
+            <p class="lead">
+              Find out about the experiment, try it out and learn how to set it up for yourself! <br>
+              You can even choose the background, the stimuli and different features to make the experiment worrespond to your needs. <br>
+              Expend the research with a quality experiment right now!!
+            </p>
+          </div>
+        </div>
+      </div>
+      <a target="_blank" rel="noopener noreferrer" href="https://www.sciencedirect.com/science/article/abs/pii/S0022103117305309">Read about the experiment here</a>
+
+
+
+      <br><br>
+      <div class="position-relative">
+        <h2 id="2-versions">The two versions</h2>
+        <a href="set-up.php#two-folders" class="open-demo top-buttons">Return to the set-up page</a>
+        <p class="lead">
+          Explicit VAAST
+        </p>
+        <p>
+          In this version, the participant responds to the stimulus the researchers are interested about.
+          For example, we could ask the participant to approach positive words and avoid negative words.
+          We'd also have them do the opposite, to measure the difference between the two conditions.
+          Here, we would be interested in whether the participant is faster in the compatible condition compared to the incompatible condition
+          (cf. approach/avoidance compatibility effects, see above article).
+        </p>
+
+        <br>
+
+        <p class="lead">Incidental VAAST</p>
+        <p>
+          In this version, the participant is asked not to pay particular attention to the element that researchers are interested about.
+          For example, we could ask the participant to approach squares and to avoid diamonds, but before each shape, a positive or a negative word would be presented.
+          The researchers would not be interested in whether the participant is faster to approach and avoid different shapes but
+          in whether they're faster to make the movement that is compatible with the word previoulsy presented (the prime).
+        </p>
+      </div>
+
+      <br>
+
+
+      <h2 id="background-images">A little note about the different backgrounds we provide</h2>
       <p class="lead">
-        Cover is a one-page template for building simple and beautiful home
-        pages. Download, edit the text, and add your own fullscreen background
-        photo to make it your own.
+        The pictures were taken in a Blender environment, every element being a 3D object.
+        The images are divided into 3 folders, depending on the visual flow and the type of environment.
+        The notion that surrounds all this how ecological these components are.
       </p>
+      <p>
+        In the <em>eco_env_vf</em> images, both the environment and the visual flow are ecological.
+        First, one could have walked through a similar street in real life.
+        Second, the changes that exist between the middle image and the approach and avoid images,
+        are the changes one experiences when making a movement towards or away from something.
+        One picture is taken as the middle image and then the camera is moved to the other positions,
+        so <strong>the visual flow is the same as one would experience when making the movement in real life</strong>.
+        <br>
+        <strong>This is the most simple folder to use.</strong>
+      </p>
+
+      <hr />
+
+      <p class="lead">
+        A few years later, an experiment was created that would offer some insight on whether one component being ecological matters more than the other being ecological.
+        The question was : For the xxcongruencexx effect, does it matter more that the environment is ecological or that the visual flow is ?
+      </p>
+
+      <p>
+        In the <em>eco_env</em> images, only the environment is ecological.
+        One could have walked through a similar street in real life.
+        But the changes that exist between the middle image and the approach and avoid images,
+        are <strong>not</strong> the changes one experiences when making a movement towards or away from something.
+        For example, some objets were made bigger or smaller between the different pictures. Also, the pictures were taken with a zoom (in and out), not with the camera moving,
+        which isn't the way we view things when we move forward or away from them.
+      </p>
+
+      <hr style="width:50%" class="mx-auto" />
+
+      <p>
+        In the <em>eco_vf</em> images, only the visual flow is ecological.
+        The environment was created so it didn't resemble anything we've encountered in real life or even in video games or media.
+        It was supposed to be an environment we'd never made approach and avoidance movements in.
+        But, the changes that exist between the middle image and the approach and avoid images,
+        <strong>are</strong> the changes one experiences when making a movement towards or away from something.
+        One picture is taken as the middle image and then the camera is moved to the other positions,
+        so <strong>the visual flow is the same as one would experience when making the movement in real life</strong>.
+      </p>
+
+      <hr />
+
+
+
+
+
+
+
+      <ol>
+        <li>First list item</li>
+        <li>Second list item with a longer description</li>
+        <li>Third list item to close it out</li>
+      </ol>
+      <p>And this is a definition list:</p>
+      <dl>
+        <dt>HyperText Markup Language (HTML)</dt>
+        <dd>
+          The language used to describe and define the content of a Web
+          page
+        </dd>
+        <dt>Cascading Style Sheets (CSS)</dt>
+        <dd>Used to describe the appearance of Web content</dd>
+        <dt>JavaScript (JS)</dt>
+        <dd>
+          The programming language used to build advanced Web sites and
+          applications
+        </dd>
+      </dl>
+      <blockquote class="blockquote">
+        <p>Quoted text goes here.</p>
+      </blockquote>
+
+      <pre><code>Example code block</code></pre>
+
+
+
       <p class="lead">
         <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
       </p>
+
     </main>
 
-    <footer class="mt-auto text-white-50">
-      <p>
-        Cover template for
-        <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>,
-        by <a href="https://x.com/mdo" class="text-white">@mdo</a>.
-      </p>
-    </footer>
+    <?php include 'footer.php'; ?>
   </div>
   <script
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
     crossorigin="anonymous"
     class="astro-vvvwv3sm"></script>
+  <script src="js/active-hd.js"></script>
 </body>
 
 </html>

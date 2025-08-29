e95b575342654d0b470b715d73504f07c97e3d0b
Maxine <maxine.audin@gmail.com>
2025-08-29 11:32:33 +0200

    different versions of the header

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<div class="container-sm">
+<!-- <div class="container-sm">
   <header class="border-bottom lh-1 py-3">
     <div class="row flex-nowrap align-items-center">
       <div class="col-4 text-center mx-auto" style="width:auto">
@@ -17,4 +17,21 @@
       <a class="nav-item nav-link link-body-emphasis" href="/contact.php">Contact</a>
     </nav>
   </div>
+</div> -->
+<div class="container-sm">
+  <header
+    class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a
+      href="index.php"
+      class="blog-header-logo d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      The Vaast Website
+    </a>
+    <nav class="nav nav-underline justify-content-between align-items-center">
+      <a class="nav-item nav-link link-body-emphasis" href="/index.php">Home</a>
+      <a class="nav-item nav-link link-body-emphasis" href="/features.php">Features</a>
+      <a class="nav-item nav-link link-body-emphasis" href="/trials.php">Try it out!</a>
+      <a class="nav-item nav-link link-body-emphasis" href="/set-up.php">Set-up</a>
+      <a class="nav-item nav-link link-body-emphasis" href="/contact.php">Contact</a>
+    </nav>
+  </header>
 </div>

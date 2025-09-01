cf4e8d21035ef4c94b129fd0acf1ae245313c385
Maxine <maxine.audin@gmail.com>
2025-09-01 10:12:04 +0200

    fix list alignment

--- footer.php
+++ footer.php
@@ -4,29 +4,21 @@
     <p class="col-md-4 mb-0 text-body-secondary">
       The VAAST Website
     </p>
-    <!-- <a
-      href="/"
-      class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
-      aria-label="Bootstrap">
-      <svg class="bi me-2" width="40" height="32" aria-hidden="true">
-        <use xlink:href="#bootstrap"></use>
-      </svg>
-    </a> -->
-    <ul class="nav col-md-4 justify-content-end">
+    <ul class="nav col-md-4 justify-content-end flex-nowrap">
       <li class="nav-item">
-        <a href="/index.php" class="nav-link px-2 text-body-secondary">Home</a>
+        <a href="/index.php" class="nav-link px-2 text-body-secondary" style="white-space: nowrap;">Home</a>
       </li>
       <li class="nav-item">
-        <a href="#/features.php" class="nav-link px-2 text-body-secondary">Features</a>
+        <a href="#/features.php" class="nav-link px-2 text-body-secondary" style="white-space: nowrap;">Features</a>
       </li>
       <li class="nav-item">
-        <a href="/trials.php" class="nav-link px-2 text-body-secondary">Try it out!</a>
+        <a href="/trials.php" class="nav-link px-2 text-body-secondary" style="white-space: nowrap;">Try it out!</a>
       </li>
       <li class="nav-item">
-        <a href="/set-up.php" class="nav-link px-2 text-body-secondary">Set-up</a>
+        <a href="/set-up.php" class="nav-link px-2 text-body-secondary" style="white-space: nowrap;">Set-up</a>
       </li>
       <li class="nav-item">
-        <a href="/contact.php" class="nav-link px-2 text-body-secondary">Contact</a>
+        <a href="/contact.php" class="nav-link px-2 text-body-secondary" style="white-space: nowrap;">Contact</a>
       </li>
     </ul>
   </footer>

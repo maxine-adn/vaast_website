c6a2807c69fa85a56f277521ed3b6c3a96229157
Maxine <maxine.audin@gmail.com>
2025-09-05 09:53:28 +0200

    remove debug lines

--- js/show-hide-help-button.js
+++ js/show-hide-help-button.js
@@ -3,25 +3,16 @@ document.querySelectorAll(".showOrHideHelp").forEach((helpButton) => {
 
   helpButton.addEventListener("click", () => {
     let whichQuestion = helpButton.id;
-    // console.log(whichQuestion)
     let classname = "qu-"+whichQuestion;
-    // console.log(classname)
     let questionP = document.getElementById(classname);
-    // console.log(questionP)
 
     if (helpIsClicked) {
-      console.log("button was clicked")
-      console.log("button"+ whichQuestion +"was clicked")
-
-
       questionP.style.display = "block";
       helpIsClicked = false;
 
       // Change l'icon
       let help_icon = document.getElementById("icon-" + whichQuestion);
-      console.log(help_icon)
       help_icon.name = "chevron-down-outline";
-      console.log(help_icon)
 
     } else {
       questionP.style.display = "none";
@@ -29,9 +20,7 @@ document.querySelectorAll(".showOrHideHelp").forEach((helpButton) => {
 
       // Change l'icon
       let help_icon = document.getElementById("icon-" + whichQuestion);
-      console.log(help_icon)
       help_icon.name = "chevron-forward-outline";
-      console.log(help_icon)
     }
   });
 });

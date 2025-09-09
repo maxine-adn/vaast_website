1bc51ff2a8292dd3b90f8d22cdad468083204361
Maxine <maxine.audin@gmail.com>
2025-09-09 09:24:24 +0200

    removed one background

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 </head>
 
 <body class="d-flex h-100"> <!-- .text-center -->
-  <?php include 'color-modes.php'; ?>
+
   <div class="cover-container d-flex w-100 mx-auto flex-column">
     <?php include 'header.php'; ?>
     <main class="container-sm px-3">
@@ -254,8 +254,15 @@
       </h1>
       <p class="lead">
         The pictures were taken in a Blender environment, every element being a 3D object.
-        The images are divided into 3 folders, depending on the visual flow and the type of environment.
-        The notion that surrounds all this how <a class="text-success-emphasis" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Coherent with real-world conditions">ecological</a> these components are.
+        The images are divided into 2 folders.
+        The notion that surrounds all this how <a class="text-success-emphasis" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Coherent with real-world conditions">ecological</a> the environment is.
+
+        One picture is taken as the middle image and then the camera is moved to the other positions,
+        so <strong>the visual flow is the same as one would experience when making the movement in real life</strong>.
+        However, the changes that exist between the middle image and the approach and avoid images,
+        <strong>are</strong> the changes one experiences when making a movement towards or away from something.
+        One picture is taken as the middle image and then the camera is moved to the other positions,
+        so <strong>the visual flow is the same as one would experience when making the movement in real life</strong>.
       </p>
 
       <div class="container-sm px-4 py-2"> <!-- col-xxl-8 -->
@@ -270,69 +277,24 @@
               loading="lazy" />
           </div>
           <div class="col-lg-6">
-            <h2>In the <em>eco_env_vf</em> images,</h2>
-            <p class="lead">
-              Both the environment and the visual flow are ecological.
-              <br>
-              First, one could have walked through a similar street in real life.
-              Second, the changes that exist between the middle image and the approach and avoid images,
-              are the changes one experiences when making a movement towards or away from something.
-              <br>
-              One picture is taken as the middle image and then the camera is moved to the other positions,
-              so <strong>the visual flow is the same as one would experience when making the movement in real life</strong>.
-            </p>
-            <h3>This is the most simple folder to use.</h3>
-          </div>
-        </div>
-      </div>
-
-      <hr style="width:50%" class="mx-auto" />
-
-      <p class="lead pt-5">
-        A few years later, an experiment was created that would offer some insight on whether one component being ecological matters more than the other being ecological.
-        The question was: For the approach/avoidance compatibility effect in the VAAST, does it matter more that the environment is ecological or that the visual flow is ?
-      </p>
-
-      <div class="container-sm px-4 py-2"> <!-- col-xxl-8 -->
-        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-          <div class="col-10 col-sm-8 col-lg-6">
-            <img
-              src="media/vaast_background_eco_env.png"
-              class="d-block mx-lg-auto img-fluid border rounded-3 p-4"
-              alt="background image with ecological environment and visual flow"
-              width="700"
-              height="500"
-              loading="lazy" />
-          </div>
-          <div class="col-lg-6">
             <h2>In the <em>eco_env</em> images,</h2>
             <p class="lead">
-              Only the environment is ecological.
+              The environment is ecological.
+              <br>
               One could have walked through a similar street in real life.
-              However, the changes that exist between the middle image and the approach and avoid images,
-              are <strong>not</strong> the changes one experiences when making a movement towards or away from something.
-              For example, some objets were made bigger or smaller between the different pictures. Also, the pictures were taken with a zoom (in and out), not with the camera moving,
-              which isn't the way we view things when we move forward or away from them.
             </p>
-            <h6 class="text-danger-emphasis">The red lines <strong>don't</strong> appear on the experiment background images. <br>
-              Here, they appear to show the elements that change in a non-ecological way.</h6>
           </div>
         </div>
       </div>
 
-
       <div class="container-sm px-4 py-2"> <!-- col-xxl-8 -->
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div class="col-lg-6">
-            <h2>In the <em>eco_vf</em> images,</h2>
+            <h2>In the <em>non_eco_env</em> images,</h2>
             <p class="lead">
-              Only the visual flow is ecological.
+              In these images, the environment is not ecological.
               The environment was created so it didn't resemble anything we've encountered in real life or even in video games or media.
               It was supposed to be an environment we'd never made approach and avoidance movements in.
-              However, the changes that exist between the middle image and the approach and avoid images,
-              <strong>are</strong> the changes one experiences when making a movement towards or away from something.
-              One picture is taken as the middle image and then the camera is moved to the other positions,
-              so <strong>the visual flow is the same as one would experience when making the movement in real life</strong>.
             </p>
           </div>
           <div class="col-10 col-sm-8 col-lg-6">

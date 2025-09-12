6d0dcfaa9676fec59aa861ca80fd51ce26e1e404
Maxine <maxine.audin@gmail.com>
2025-09-12 11:17:31 +0200

    adding active class to home button with just / in url

--- js/active-hd.js
+++ js/active-hd.js
@@ -1,7 +1,9 @@
 // add active class to the current page title in the nav-bar
 
 document.querySelectorAll(".nav-link").forEach((link) => {
-  if (link.href === window.location.href) {
+  const linkUrl = link.href.replace(/\/index\.php$/, '/');
+  const currentUrl = window.location.href.replace(/\/index\.php$/, '/');
+  if (linkUrl === currentUrl) {
     link.classList.add("active");
   }
 });
